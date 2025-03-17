a20b89032e051155eaf6d3c20818f0b4f66d86f5
Abhivarma Birru <abhivarma.birru@gmail.com>
2025-03-17 02:47:41 -0700

    local-development-branch

--- Backend/index.js
+++ Backend/index.js
@@ -9,28 +9,16 @@ dotenv.config(); // Load .env file into process.env
 
 const app = express();
 
-let allowedOrigins = [];
-if (process.env.ALLOWED_ORIGINS) {
-  try {
-    // Try to parse the env variable as JSON, e.g. '["https://delivery-mapping-application-abv.vercel.app","http://localhost:5174/"]'
-    allowedOrigins = JSON.parse(process.env.ALLOWED_ORIGINS);
-  } catch (error) {
-    // Fallback: split by comma and trim spaces if JSON parsing fails
-    allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim());
-  }
-} else {
-  allowedOrigins = [];
-}
-console.log('Allowed origins:', allowedOrigins);
+console.log('Allowed origins:', process.env.ALLOWED_ORIGINS);
 
 app.use(cors({
   origin: (origin, callback) => {
-    console.log("origigigi", origin)
-    // If no origin is provided (like in some server-to-server requests), allow it
+    console.log("origin", origin)
+    // If no origin is provided (like in some server-to-server requests),
+    //  allow it - Only for Local Developmet!!!!!
     if (!origin) return callback(null, true);
 
-    // If the origin is in the allowedOrigins array, allow it
-    if (allowedOrigins.includes(origin)) {
+    if (process.env.ALLOWED_ORIGINS.includes(origin)) {
       return callback(null, true);
     } else {
       return callback(new Error('Not allowed by CORS'));
