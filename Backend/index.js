c7ea19646f6e7322f74d73d2dbd8e79f11389a87
Abhivarma Birru <abhivarma.birru@gmail.com>
2025-03-17 01:39:09 -0700

    pushed code to main

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import distanceRoute from './routes/distanceRoute.js';
 import historyRoute from './routes/historyRoute.js';
-import { connectDB } from './config/db.js';
 
 dotenv.config(); 
 
@@ -45,7 +44,6 @@ app.use('/distance', distanceRoute); // To use distance route for distance calcu
 app.use('/history', historyRoute); // To retrive history of distance calculation for previous queries
 
 app.get('/', async (req, res) => {
-    await connectDB(); // Connect to the database
     res.status(200).send('Backend Started Running!!!!');
 });
 app.use((err, req, res, next) => {
